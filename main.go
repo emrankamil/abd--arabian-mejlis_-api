b4e469669f23abbe12c9b8c95f0df3a74a4fb609
emran <emranhi001@gmail.com>
2024-09-26 02:57:20 +0300

    changed port binding

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"abduselam-arabianmejlis/domain"
 	"time"
 
+	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -31,6 +33,17 @@ func main() {
 	timeout := time.Duration(env.ContextTimeout) * time.Second
 
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+    config.AllowAllOrigins = true
+    config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS", "DELETE"}
+    config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+    config.ExposeHeaders = []string{"Content-Length"}
+    config.AllowCredentials = true
+    config.MaxAge = 12 * time.Hour
+
+    router.Use(cors.New(config))
+
 	route.Setup(env, timeout, db, router, nil, clientManager)
 
 	// Setup WebSocket routes using Gin
